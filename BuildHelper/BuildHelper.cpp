65d31e8df7ec12f84bd9eb98b96fee673eabe002
kys0522 <kys0522@midasit.com>
2019-05-21 14:14:15 +0900

    ㄹㄴㅇㄹㄴㅇㄹ

--- BuildHelper/BuildHelper.cpp
+++ BuildHelper/BuildHelper.cpp
@@ -30,6 +30,8 @@ CBuildHelperApp::CBuildHelperApp()
 	// 다시 시작 관리자 지원
 	m_dwRestartManagerSupportFlags = AFX_RESTART_MANAGER_SUPPORT_RESTART;
 
+	int a = 3;
+	a++;
 	// TODO: 여기에 생성 코드를 추가합니다.
 	// InitInstance에 모든 중요한 초기화 작업을 배치합니다.
 	m_dwRestartManagerSupportFlags = AFX_RESTART_MANAGER_SUPPORT_ALL_ASPECTS;
