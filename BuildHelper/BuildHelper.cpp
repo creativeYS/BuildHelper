fadd2adc8fbdb1ece824c6cf947449773bf6ceac
kys0522 <kys0522@midasit.com>
2019-05-22 17:01:40 +0900

    a

--- BuildHelper/BuildHelper.cpp
+++ BuildHelper/BuildHelper.cpp
@@ -157,6 +157,7 @@ BOOL CBuildHelperApp::InitInstance()
 	// 대화 상자가 닫혔으므로 응용 프로그램의 메시지 펌프를 시작하지 않고  응용 프로그램을 끝낼 수 있도록 FALSE를
 	// 반환합니다.
 	return FALSE;
+	a
 }
 
 int CBuildHelperApp::ExitInstance()
